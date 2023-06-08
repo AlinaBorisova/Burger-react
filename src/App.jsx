4187669775b626cc4a849fb0abb6aeae982451e9
Алина Борисова <b-alina1999.borisova@yandex.ru>
2023-06-08 18:29:29 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {Container} from "./components/Container/Container.jsx";
+import {Header} from "./components/Header/Header.jsx";
+
+export const App = () => {
+
+  return (
+      < >
+          <Header />
+          <main>
+              <nav>
+                  <Container />
+              </nav>
+              <section></section>
+          </main>
+          <footer></footer>
+      < / >
+  )
+}
